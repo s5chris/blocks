0c3356b96973dd68ee1be842021447872f0b49b7
unknown <chris@split5.com>
2009-01-25 17:58:46 -0700

    Simplified the English for the form submission successs msg

--- view.php
+++ view.php
@@ -14,7 +14,7 @@ $miniSurvey->frontEndMode=true;
 <form id="miniSurveyView<?=intval($survey->questionSetId)?>" class="miniSurveyView" method="post" action="<?=$this->action('submit_form')?>">
 	<div style="margin-bottom:8px"><strong><?=$survey->surveyName?></strong></div>
 	<? if( $_GET['surveySuccess'] && $_GET['qsid']==intval($survey->questionSetId) ){ ?>
-		<div id="msg">Form Submission Successful.</div> 
+		<div id="msg">Thank you.</div> 
 	<? } ?>
 	<input name="qsID" type="hidden" value="<?=intval($survey->questionSetId)?>" />
 	<input name="pURI" type="hidden" value="<?=$_SERVER['REQUEST_URI']?>" />
