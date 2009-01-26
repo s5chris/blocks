48e5472ec68fba5a9643d5c53ad1d82882a8b68f
unknown <chris@split5.com>
2009-01-25 17:57:15 -0700

    Deleted Style Sheet

--- view.php
+++ view.php
@@ -4,17 +4,12 @@ $survey=$controller;
 $miniSurvey=new MiniSurvey($b);
 $miniSurvey->frontEndMode=true;
 ?>
-<style>
-.miniSurveyView{ margin-bottom:16px}
-.miniSurveyView padding:2px; border:1px solid #999; margin:8px 0px 8px 0px;}
-.miniSurveyView table.formBlockSurveyTable td{ padding-bottom:4px;  width: 262px; clear: both; }
-.miniSurveyView td.question {padding-right: 1px; width: 262px; clear:both;}
-</style>
+
 
 <form id="miniSurveyView<?=intval($survey->questionSetId)?>" class="miniSurveyView" method="post" action="<?=$this->action('submit_form')?>">
 	<div style="margin-bottom:8px"><strong><?=$survey->surveyName?></strong></div>
 	<? if( $_GET['surveySuccess'] && $_GET['qsid']==intval($survey->questionSetId) ){ ?>
-		<div id="msg">Form Submission Successful.</div> 
+		<div id="msg">Form Submission Was Successful.</div> 
 	<? } ?>
 	<input name="qsID" type="hidden" value="<?=intval($survey->questionSetId)?>" />
 	<input name="pURI" type="hidden" value="<?=$_SERVER['REQUEST_URI']?>" />
